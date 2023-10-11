1f9683ae8e8b79672157f04fa8caa36750a62768
05WilliamCH <wchiroym@miumg.edu.gt>
2023-10-11 01:25:09 -0600

    login_v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
-import { useState } from "react";
-import Login from "./pages/Login";
 import Inicio from "./pages/Inicio";
+import Login from "./pages/Login";
 import Proveedor from "./pages/Proveedor";
 import Usuario from "./pages/Usuario";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes, } from "react-router-dom";
+
+import { useEffect, useState } from 'react';
 
 function App() {
   //const [count, setCount] = useState(0)
+    /* Lógica de autenticación de Usuario */
+    const [isAuthenticated, setIsAuthenticated] = useState(true);//Cambiar false
+    const setAuth = (booleand) => {
+      setIsAuthenticated(booleand);
+    };
+  
+    async function isAuth() {
+      try {
+        const response = await fetch("http://localhost:3000/auth/verify", {
+          method: "GET",
+          headers: { token: localStorage.token },
+        });
+  
+        const parseRes = await response.json();
+        parseRes === true ? setIsAuthenticated(true) : setIsAuthenticated(false);
+      } catch (err) {
+        console.error(err.message);
+      }
+    }
+  
+    useEffect(() => {
+      isAuth();
+    });
 
   return (
     <>
